4db41df76cb45532b830cfa63a0d47b1b411b07f
Daniel Roy Greenfeld <daniel@feldroy.com>
2025-11-26 09:51:08 +0800

    Add justfile
    Co-authored-by: Audrey Roy Greenfeld <audrey@feldroy.com>

--- tests/test_tags.py
+++ tests/test_tags.py
@@ -2,5 +2,8 @@ import airdragon as ad
 
 
 def test_H1():
-    html = ad.H1('Hello, world.').render()
-    assert html == '<h1 class="text-3xl sm:text-4xl font-semibold leading-tight">Hello, world.</h1>'
+    html = ad.H1("Hello, world.").render()
+    assert (
+        html
+        == '<h1 class="text-3xl sm:text-4xl font-semibold leading-tight">Hello, world.</h1>'
+    )
